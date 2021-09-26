caf449a1591142f4615d133c5faf5c266d3f7b12
David Pochet <david.pochet@gmail.com>
2021-09-26 16:48:57 +0200

    Event resume handling -- resolves #56

--- src/tests/test_events.py
+++ src/tests/test_events.py
@@ -2,9 +2,11 @@ import threading
 import time
 from queue import Empty, Queue
 
-from grpc_helper.api import Event, EventInterrupt, EventProperty, Filter, ResultCode
+from grpc_helper.api import Empty as EmptyMessage
+from grpc_helper.api import Event, EventFilter, EventInterrupt, EventProperty, ResultCode
 from grpc_helper.api.events_pb2 import EventStatus
 from grpc_helper.errors import RpcException
+from grpc_helper.static_config import RpcStaticConfig
 from tests.utils import TestUtils
 
 
@@ -12,7 +14,7 @@ class TestEvents(TestUtils):
     def test_listen_missing_params(self, client):
         # Try with bad filters
         try:
-            for _ in client.events.listen(Filter(names=[""])):
+            for _ in client.events.listen(EventFilter(names=[""])):
                 pass
             raise AssertionError("shouldn't get here")
         except RpcException as e:
@@ -21,11 +23,20 @@ class TestEvents(TestUtils):
     def test_listen_bad_params(self, client):
         # Try with bad filters
         try:
-            for _ in client.events.listen(Filter(names=["with space"])):
+            for _ in client.events.listen(EventFilter(names=["with space"])):
                 pass
             raise AssertionError("shouldn't get here")
         except RpcException as e:
             assert e.rc == ResultCode.ERROR_PARAM_INVALID
+
+    def test_listen_unknown(self, client):
+        # Try with unknown client
+        try:
+            for _ in client.events.listen(EventFilter(client_id=135)):
+                pass
+            raise AssertionError("shouldn't get here")
+        except RpcException as e:
+            assert e.rc == ResultCode.ERROR_ITEM_UNKNOWN
 
     def test_send_missing_params(self, client):
         # Try with bad event
@@ -51,20 +62,27 @@ class TestEvents(TestUtils):
         except RpcException as e:
             assert e.rc == ResultCode.ERROR_ITEM_UNKNOWN
 
-    def listening_thread(self, client, start_event: threading.Event, rec_queue: Queue):
-        # Ready to listen
-        start_event.set()
+    def listening_thread(self, client, start_event: threading.Event, rec_queue: Queue, index: int, except_while_listening: bool):
         try:
-            for s in client.events.listen(Filter(names=["some-event"])):
+            # Listen to events
+            for s in client.events.listen(EventFilter(names=["some-event"], client_id=index)):
+                start_event.set()
                 rec_queue.put_nowait(s)
+
+                # Interrupt with exception is required
+                if len(s.event.name) and except_while_listening:
+                    raise Exception("Some unknown exception")
         except RpcException as e:
             rec_queue.put_nowait(e)
 
-    def start_listening(self, client):
+    def start_listening(self, client, index: int = None, q: Queue = None, except_while_listening: bool = False):
         # Start listening thread
         start_event = threading.Event()
-        rec_queue = Queue()
-        t = threading.Thread(target=self.listening_thread, kwargs={"client": client, "start_event": start_event, "rec_queue": rec_queue})
+        rec_queue = Queue() if q is None else q
+        t = threading.Thread(
+            target=self.listening_thread,
+            kwargs={"client": client, "start_event": start_event, "rec_queue": rec_queue, "index": index, "except_while_listening": except_while_listening},
+        )
         t.start()
         start_event.wait()
         time.sleep(0.2)
@@ -89,8 +107,21 @@ class TestEvents(TestUtils):
             assert s.client_id > 0
             assert s.event.name == ""
 
+            # Verify inspect contains id
+            assert s.client_id in client.events.inspect(EmptyMessage()).client_ids
+
             # Interrupt
             client.events.interrupt(EventInterrupt(client_id=s.client_id))
+
+            # Verify inspect still contains id
+            assert s.client_id in client.events.inspect(EmptyMessage()).client_ids
+
+            # Try to interrupt again (expecting bad state)
+            try:
+                client.events.interrupt(EventInterrupt(client_id=s.client_id))
+                raise AssertionError("shouldn't get here")
+            except RpcException as e:
+                assert e.rc == ResultCode.ERROR_STATE_UNEXPECTED
 
             # Join listening thread and flush all received events
             t.join()
@@ -132,3 +163,75 @@ class TestEvents(TestUtils):
         e = all_status[1]
         assert isinstance(e, RpcException)
         assert e.rc == ResultCode.ERROR_STREAM_SHUTDOWN
+
+    def test_inspect_empty(self, client):
+        # By default, no running event loops
+        s = client.events.inspect(EmptyMessage())
+        assert len(s.client_ids) == 0
+
+    def test_resume(self, client):
+        # Start listening
+        rec_queue, t = self.start_listening(client)
+
+        # Get client ID
+        s = rec_queue.get()
+        index = s.client_id
+
+        # Send one before interrupting
+        client.events.send(Event(name="some-event"))
+
+        # Interrupt
+        client.events.interrupt(EventInterrupt(client_id=index))
+        t.join()
+
+        # Send one while interrupted
+        client.events.send(Event(name="some-event"))
+
+        # Resume
+        _, t = self.start_listening(client, index, rec_queue)
+
+        # Send one after resume
+        client.events.send(Event(name="some-event"))
+
+        # Final interrupt
+        RpcStaticConfig.EVENT_RETAIN_TIMEOUT.update("1")
+        client.events.interrupt(EventInterrupt(client_id=index))
+        t.join()
+
+        # Wait for retain timeout to expire
+        time.sleep(1.5)
+
+        # Send one after timeout
+        client.events.send(Event(name="some-event"))
+
+        # Interrupt again (should be unknown)
+        try:
+            client.events.interrupt(EventInterrupt(client_id=index))
+            raise AssertionError("shouldn't get here")
+        except RpcException as e:
+            assert e.rc == ResultCode.ERROR_ITEM_UNKNOWN
+
+        # Check final event count
+        events = self.flush_queue(rec_queue)
+        assert len(events) == 4  # one with resume id (no event) + 3 really received ones
+        assert all(e.client_id == index for e in events)
+        with_events = list(filter(lambda e: e.event.name != "", events))
+        assert len(with_events) == 3
+        assert all(e.event.name == "some-event" for e in with_events)
+
+    def test_client_interrupt(self, client):
+        # Start listening (asking to kill listening loop on client side)
+        rec_queue, t = self.start_listening(client, except_while_listening=True)
+
+        # Send event
+        client.events.send(Event(name="some-event"))
+
+        # Thread shall be terminated
+        t.join()
+
+        # Send event (should be interrupted)
+        client.events.send(Event(name="some-event"))
+
+        # Flush events (2 expected)
+        events = self.flush_queue(rec_queue)
+        assert len(events) == 2
